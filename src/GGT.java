bb99636532286fe8096f82566dca6363d2189ea2
D062618 <sebastian.roehling@sap.com>
2016-09-08 11:09:25 -0700

    added do while loop

--- src/GGT.java
+++ src/GGT.java
@@ -11,37 +11,44 @@ public class GGT {
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("Bitte geben Sie zwei Zahlen ein, von denen der größte gemeinsame Teiler ermittelt werden soll");
-		System.out.println("1. Zahl:");
-		x = scanner.nextInt();
-		
-		System.out.println("2. Zahl:");
-		y = scanner.nextInt();
-		
-		if (x > y) {
-			ggt = y;
-		}else {
-			ggt = x;
-		}
-		
-		boolean b = false;
-		/*do {
-			b = (x % ggt == 0 && y % ggt == 0);
+		do{
+					
+			System.out.println("Bitte geben Sie zwei Zahlen ein, von denen der größte gemeinsame Teiler ermittelt werden soll");
+			System.out.println("1. Zahl:");
+			x = scanner.nextInt();
 			
-			if(!b){
-				ggt = ggt - 1;
-			}
-		} while (!b);*/
-		
-		while(!b){
-			b = (x % ggt == 0 && y % ggt == 0);
+			System.out.println("2. Zahl:");
+			y = scanner.nextInt();
 			
-			if(!b){
-				ggt = ggt - 1;
+			if (x > y) {
+				ggt = y;
+			}else {
+				ggt = x;
 			}
-		};
+			
+			boolean b = false;
+			
+			//Lösung mit Do-Schleife
+			/*do {
+				b = (x % ggt == 0 && y % ggt == 0);
+				
+				if(!b){
+					ggt = ggt - 1;
+				}
+			} while (!b);*/
+			
+			//Lösung mit While-Schleife
+			while(!b){
+				b = (x % ggt == 0 && y % ggt == 0);
+				
+				if(!b){
+					ggt = ggt - 1;
+				}
+			};
+			
+			System.out.println("Der größte gemeinsame Teiler ist: " + ggt);
 		
-		System.out.println("Der größte gemeinsame Teiler ist: " + ggt);
+		}while(true);
 		
 	}
 
