9265c13d8deffd2e5326840171aa6e37c3f05dc3
D062618 <sebastian.roehling@sap.com>
2016-09-08 14:17:44 -0700

    laufspiel

---
+++ src/Laufspiel.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+
+public class Laufspiel {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		
+		//Zielposition
+		int a = (int)Math.round(1 + Math.random()*6);
+		int b = (int)Math.round(1 + Math.random()*6);
+		
+		//Startposition
+		int x;
+		int y;
+		//Ermittle solange eine neue Startposition, bis die Startposition nicht gleich der Zielposition ist
+		do{
+			x = (int)Math.round(1 + Math.random()*6);
+			y = (int)Math.round(1 + Math.random()*6);
+		}while(x == a && y == b);
+		
+		//Versuchezähler
+		int versuche = 0;
+		
+		//Zum testen kannst du dir die Start und Zielpositionen ausgeben lassen ;)
+		//System.out.println("Startposition: (" + x + ", " + y + ")");
+		//System.out.println("Zielposition: (" + a + ", " + b + ")");
+		
+		//Solange die Positionen nicht übereinstimmen, führe die While Schleife aus
+		while(!(x == a && y == b)){
+			//Berechne die Distanz zwischen den beiden Punkten und gebe diese aus
+			double abstand = Math.sqrt( Math.pow((a - x), 2) + Math.pow((b - y), 2) );
+			System.out.println("Die Distanz zwischen Start und Ziel beträgt " + abstand);
+			
+			System.out.println("In welche Richtung möchtest du laufen (1 - nach oben | 2 - nach unten | 3 - nach links | 4 - nach rechts)?");
+			int richtung = scanner.nextInt();
+			versuche = versuche + 1;
+			
+			if(richtung == 1){
+				//Verhindere, dass der Spieler aus dem Spielfeld herausläuft
+				if(y < 7){
+					y = y + 1;
+					System.out.println("Du läufst eins nach oben");
+				}else{
+					System.out.println("Du bist schon am oberen Rand. Du kannst nicht noch weiter nach oben.");
+				}
+				
+			}else if(richtung == 2){
+				//Verhindere, dass der Spieler aus dem Spielfeld herausläuft
+				if(y > 1){
+					y = y - 1;
+					System.out.println("Du läufst eins nach unten");
+				}else{
+					System.out.println("Du bist schon am unteren Rand. Du kannst nicht noch weiter nach unten.");
+				}
+				
+			}else if(richtung == 3){
+				//Verhindere, dass der Spieler aus dem Spielfeld herausläuft
+				if(x > 1){
+					x = x - 1;
+					System.out.println("Du läufst eins nach links");
+				}else{
+					System.out.println("Du bist schon am linken Rand. Du kannst nicht noch weiter nach links.");
+				}
+				
+			}else if(richtung == 4){
+				//Verhindere, dass der Spieler aus dem Spielfeld herausläuft
+				if(x < 7){
+					x = x + 1;
+					System.out.println("Du läufst eins nach rechts");
+				}else{
+					System.out.println("Du bist schon am rechten Rand. Du kannst nicht noch weiter nach rechts.");
+				}
+
+			}else{
+				System.out.println("Unzulässige Eingabe.");
+			}
+			
+			System.out.println("Position: (" + x + ", " + y + ")");
+		}
+		
+		System.out.println("-----------------------------------------------------------------------------");
+		System.out.println("Glückwunsch, du hast das Ziel erreicht. Du hast " + versuche + " Versuche gebraucht. Party!");
+		System.out.println("-----------------------------------------------------------------------------");
+		
+	}
+
+}
