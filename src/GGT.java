6a6b9153719d7d11b9fe07c0083f835d7d155359
D062618 <sebastian.roehling@sap.com>
2016-09-08 10:53:01 -0700

    ggt

---
+++ src/GGT.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class GGT {
+
+	public static void main(String[] args) {
+
+		//Zahl 1 und Zahl 2
+		int x;
+		int y;
+		int ggt;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Bitte geben Sie zwei Zahlen ein, von denen der größte gemeinsame Teiler ermittelt werden soll");
+		System.out.println("1. Zahl:");
+		x = scanner.nextInt();
+		
+		System.out.println("2. Zahl:");
+		y = scanner.nextInt();
+		
+		if (x > y) {
+			ggt = y;
+		}else {
+			ggt = x;
+		}
+		
+		boolean b = false;
+		/*do {
+			b = (x % ggt == 0 && y % ggt == 0);
+			
+			if(!b){
+				ggt = ggt - 1;
+			}
+		} while (!b);*/
+		
+		while(!b){
+			b = (x % ggt == 0 && y % ggt == 0);
+			
+			if(!b){
+				ggt = ggt - 1;
+			}
+		};
+		
+		System.out.println("Der größte gemeinsame Teiler ist: " + ggt);
+		
+	}
+
+}
